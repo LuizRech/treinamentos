26d4c1049e16b615a9ae3ffa5be334f6367514d0
Luiz Rech <luizadre@hotmail.com>
2019-10-02 19:10:23 -0300

    Primeiro commit

---
+++ meujs.js
@@ -0,0 +1,11 @@
+$(function(){
+// Ação que vai executar depois de carregar toda a pagina
+// JQUERY
+
+
+});
+
+function imagem(id){
+	$("#imagem > .img").hide();
+	$("#imagem > #imagem_"+id).show();
+}
